8e7d4ac834248fdcfdd8591aa7d97d497bf9bea6
frankbriones <frankbriones90@gmail.com>
2019-11-07 18:07:33 -0500

    cambios setting9

--- fund5/local_settings.py
+++ fund5/local_settings.py
@@ -4,19 +4,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'FND5',
-        'USER': 'postgres',
-        'PASSWORD': 'qwerty',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',      
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
-
-SECRET_KEY = '-v=q6ry5d8=bcpczvblq68xc(n*hg0#_9b4p65(tc162f#0nb*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
