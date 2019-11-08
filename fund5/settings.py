97ad1bc2171e2a7bfc25f80b97c9be03372c3306
frankbriones <frankbriones90@gmail.com>
2019-11-07 19:20:12 -0500

    cambios setting9

--- fund5/settings.py
+++ fund5/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django-heroku
 # # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -248,3 +248,7 @@ SESSION_SAVE_EVERY_REQUEST = True
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+
+django_heroku.settings(locals())
